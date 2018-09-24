ab3a9bbaa329fc6dcb7efa61b2e5db8974dccccd
Deborah Oni <deborah.oni@andela.com>
2018-09-24 13:20:38 +0100

    error handling and testing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/users')
+const router = express.Router;
 
 const app = express();
 
@@ -24,16 +24,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {
+    stack: err.status === 404 ? 'Page not found!' : 'Error occured!'
+  };
 
   // render the error page
   res.status(err.status || 500);
